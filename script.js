e1611ba9c4aa4808769af8b0cbda570bd693f60d
Amil Hasbala <amilhasbala@gmail.com>
2019-10-25 16:38:47 +0700

    update script

--- script.js
+++ script.js
@@ -5,28 +5,72 @@ const sentences = [
 ];
 
 // pick a sentence and split into words
-arrWords = sentences[0].split(" ");
+var arrWords = sentences[0].split(" ");
 
-// display the words to the page
+// THE SENTENCE
 // DOM element
-const gameSentence = document.querySelector("#game-sentence");
+const gameSentence = document.getElementById("game-sentence");
 
 // Loop arr words
 for (let i = 0; i < arrWords.length; i++) {
-  gameSentence.innerHTML += `<span class="word">${arrWords[i]}</span>`;
+  gameSentence.innerHTML += `<span class="word" id="word${i}">${arrWords[i]}</span>`;
 }
 
-// timer
+// THE TIMER
 // DOM element
-var timerDisplay = document.querySelector("#timer");
+var timerDisplay = document.getElementById("timer");
 
-var seconds = 0;
+var seconds = 0,
+  intervalTime,
+  intervalStatus;
 // function timer start on body load
 function timerStart() {
-  console.log("timer start");
   // add 1 every second
-  setInterval(function() {
+  // interval for timer
+  intervalTime = setInterval(function() {
     seconds++;
     timerDisplay.innerHTML = seconds;
+    speed.innerHTML = `${Math.round((60 / seconds) * score * 10) /
+      10} words per minute`;
   }, 1000);
+  // interval for game status
+  intervalStatus = setInterval(checkStatus, 50);
+}
+
+// THE SCORE
+//DOM element
+const scoreElement = document.getElementById("accuracy");
+const speed = document.getElementById("speed");
+
+// THE GAME
+//DOM element
+const wordInput = document.getElementById("word-input");
+
+var wordCounter = 0,
+  score = 0;
+
+// listen for "space bar"
+wordInput.addEventListener("keyup", event => {
+  if (event.code === "Space") {
+    if (wordInput.value.trim() === arrWords[wordCounter]) {
+      score++;
+    }
+    document.getElementsByClassName("active")[0].className = "word done";
+    scoreElement.innerHTML = `${Math.round(
+      (score / (wordCounter + 1)) * 100
+    )} %`;
+    wordCounter++;
+    wordInput.value = "";
+  }
+});
+
+// CURRENT WORD & END GAME
+function checkStatus() {
+  if (wordCounter === arrWords.length) {
+    clearInterval(intervalTime);
+    clearInterval(intervalStatus);
+  } else {
+    var current = document.getElementById(`word${wordCounter}`);
+    current.className = "word active";
+  }
 }
